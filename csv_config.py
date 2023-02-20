056992c68a9dba7e392e8e93103ae4db595871c7
DarkHost <sultanisk@mail.ru>
2023-02-20 21:02:22 +0600

    Adding ipynb file and some fixes

--- csv_config.py
+++ csv_config.py
@@ -5,7 +5,7 @@ import csv
 def parseResume(job):
     links = getLink(job)
 
-    with open(job + '.csv', 'w+', newline='') as f:
+    with open(job + '.csv', 'w+', newline='', encoding='utf-8') as f:
         resume = dict(parseLink(links[0]))
         csvControl = csv.writer(f)
         csvControl.writerow(resume.keys())
@@ -13,7 +13,7 @@ def parseResume(job):
 
         for _ in range(1, len(links)):
             resume = dict(parseLink(links[_]))
-            with open(job + '.csv', 'a+', newline='' ) as f:
+            with open(job + '.csv', 'a+', newline='', encoding='utf-8' ) as f:
                 csvControl = csv.writer(f)
                 csvControl.writerow(resume.values())
 
